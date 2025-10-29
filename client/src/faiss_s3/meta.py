f655a7b185a0c38da7fee42f6798920e0473310d
Pinglei Guo <at15@outlook.com>
2025-10-28 21:17:03 -0700

    client: Use IO_FLAG_MMAP
    Cannot use IO_FLAG_SKIP_IVF_DATA directly ...

--- client/src/faiss_s3/meta.py
+++ client/src/faiss_s3/meta.py
@@ -13,7 +13,9 @@ class IVFIndexMetadata:
 
 def generate_meta_from_file(index_file_path: str) -> IVFIndexMetadata:
     file_size = os.path.getsize(index_file_path)
-    # TODO: Should we use mmap or SKIP_IVF_DATA?
+    # NOTE: IO_FLAG_SKIP_IVF_DATA cannot be used directly.
+    # Need to use IO_FLAG_MMAP to load ArrayInvertedLists as OnDiskInvertedLists.
+    # https://github.com/facebookresearch/faiss/blob/2cf82cabf2b2150ca76b9949377b484f109a94d1/faiss/index_io.h#L59-L61
     index = faiss.read_index(index_file_path, faiss.IO_FLAG_MMAP)
     return _generate_meta(index, file_size)
 
